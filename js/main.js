a8338dd371657ba369077e0157df8987e3d7c4ac
Farah <fn.rusli@outlook.com>
2022-05-11 17:29:17 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function getFetch(){
+  const url = ``
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
